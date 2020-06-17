ce3fcb21692b2335aa9c30293a7df9d93c519ab7
hongwei <295606750@qq.com>
2020-06-17 11:17:22 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <Header title="我的熊猫电影" />
+    <div id="content">
+      <Login />
+      <!-- <router-view /> -->
+    </div>
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+import Login from "@/components/Login";
+export default {
+  name: "Mine",
+  components: {
+    Header,
+    TabBar,
+    Login,
+  },
+};
+</script>
+
+<style></style>
